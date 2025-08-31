76c08ddaa18838dba3f29132a93f216e46a79435
ArneBanane187 <jon.weilbaecher@web.de>
2025-08-31 15:12:35 +0200

    Nur noch aufgabe 9

--- Schule/src/blatt01/TypKonversion.java
+++ Schule/src/blatt01/TypKonversion.java
@@ -21,7 +21,14 @@ public class TypKonversion {
         System.out.println(g);
         System.out.println(h);
 
+        boolean t = true;
+        h = (char) t;
+
 
 
     }
 }
+//b) b macht aus der Kommazahl -21.25 eine ganze Zahl -21. A macht aus einer ganzen Zahl 14 eine Kommazahl 14.0. Ohne int gäbe es einen Fehler ohne double würde es trotzdem funktionieren.
+//c) Int speichert nur ganze Zahlen. Während double auch ganze Zahlen speichert, kann es auch Kommazahlen speichern.
+//d) Char ist ein Zeichen, das als Zahl im Unicode gespeichert ist. Int ist eine Zahl. Man kann einen Char in eine Zahl umwandeln und eine Zahl in einen Char umwandeln.
+//e) Boolean kann man in Java nicht in einen Char umwandeln. Deshalb gibt der Kompiler einen Fehler.
