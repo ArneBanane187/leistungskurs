1a8e0373d737e5e3350181d28bae47d56cdca9a4
JonValentinWeilbäche <jon.weilbaecher@web.de>
2025-09-24 11:24:23 +0200

    blatt06 weitermachen

---
+++ Schule/src/blatt06/Wertetabelle.java
@@ -0,0 +1,36 @@
+package blatt06;
+
+import java.util.Scanner;
+
+public class Wertetabelle {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Geben Sie Ihren Grad ein: ");
+        int grad = input.nextInt();
+
+        System.out.println("Geben Sie Ihre Koeffizienten ein: ");
+        double[] coeff = new double[grad+1];
+
+        for (int i = 0; i < coeff.length; i++) {
+            System.out.println("Koeffizient " + i);
+            coeff[i] =  input.nextDouble();
+        }
+
+
+        //Beispiel x = 1
+
+        int x = 1;
+        double y = 0;
+        for (int i = 0; i < coeff.length; i++) {
+            y = y + (coeff[i] * Math.pow(x,i));
+        }
+        System.out.println("Für x = " + x + " ist y = " + y);
+
+
+
+
+
+
+    }
+}
