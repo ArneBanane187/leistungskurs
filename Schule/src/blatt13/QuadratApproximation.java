57884d5df1dcf22a103bb3e9dd871da9e10891fe
ArneBanane187 <jon.weilbaecher@web.de>
2025-11-25 17:01:25 +0100

    blatt13 muss fertig

--- Schule/src/blatt13/QuadratApproximation.java
+++ Schule/src/blatt13/QuadratApproximation.java
@@ -1,6 +1,7 @@
 package blatt13;
 
 public class QuadratApproximation {
+    //a)
     /**
      * Funktion die einen Flaecheninhalt von beliebigen Seitenlaengen eines Quadrats berechnet
      * @param a berechnet den Flächeninhalt
@@ -10,13 +11,20 @@ public class QuadratApproximation {
         int flaecheninhalt = (int) Math.pow (a,2);
         return flaecheninhalt;
     }
+    //b)
 
+    /**
+     * Funktion die, die Seitenlänge aus einem Flächeninhalt eines Quadrates appromativ errechnet.
+     * @param a
+     * @return
+     */
     public static int sucheSeitenlaenge(int a){
-
-
+        return (int) Math.sqrt(a);
     }
+
 
     public static void main(String[]arcs){
         System.out.println(flaecheQuardrat(6));
+        System.out.println(sucheSeitenlaenge(36));
     }
 }
