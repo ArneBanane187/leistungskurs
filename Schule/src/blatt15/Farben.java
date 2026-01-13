a1e8c195b3711b7f6a8c5c2b1b1b5f9923be9295
JonValentinWeilbäche <jon.weilbaecher@web.de>
2026-01-13 13:06:00 +0100

    blatt14 yallah

---
+++ Schule/src/blatt15/Farben.java
@@ -0,0 +1,14 @@
+package blatt15;
+
+import schisch_visualizer.SchischVisualizer;
+
+public class Farben {
+
+    static char[][] spielfeld;
+    static int[][] spielerPosX;
+    static int[][] spielerPosY;
+
+    public static void main (String[] args) {
+        SchischVisualizer sv = new SchischVisualizer();
+    }
+}
