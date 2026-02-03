866ae1587a041b5aa69be82f1a4984855687a3fc
JonValentinWeilbäche <jon.weilbaecher@web.de>
2026-02-03 13:06:16 +0100

    blatt15 yallah

---
+++ Schule/src/blatt16/Systemsprenger.java
@@ -0,0 +1,20 @@
+package blatt16;
+
+import java.nio.file.Files;
+
+public class Systemsprenger {
+
+    public static void sprengen(int Anzahl){
+        try{
+            if(f.notExists()) {
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        File f = new File("Schule/Sprengtest");
+
+
+    }
+}
