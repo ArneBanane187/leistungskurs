2f1e15ce8fbae18d257d28d6d6e495658bd897d7
ArneBanane187 <jon.weilbaecher@web.de>
2026-02-24 21:23:00 +0100

    blatt17 aufgehts

--- Schule/src/blatt17/GameOfLife.java
+++ Schule/src/blatt17/GameOfLife.java
@@ -13,14 +13,58 @@ public class GameOfLife {
         blatt14.Simulationen.fuellen(spielfeld, '9', a);
     }
 
-    //Regel 1: Eine tote Zelle mit genau drei lebenden Nachbarn wird in der Folgegeneration neu geboren.
+    //Regeln der Zellen
+
+    public static void calculateNextGeneration(int z, int s, char[][] spielfeld){
+        char[][] nextSpielfeld = new char[spielfeld.length][spielfeld[0].length];
+
+        for(int i = 0; i < spielfeld.length; i++){
+            for(int j = 0; j < spielfeld[0].length; j++){
+
+                int lebendeNachbarzelle = countLebendeNachbarzellen(z, s, spielfeld);
+
+                boolean lebt = (spielfeld[i][j] == '9');
+                //Regel 1
+                if(!lebt && lebendeNachbarzelle == 3){
+                    nextSpielfeld[i][j] = '9';
+                //Regel 2
+                } else if(lebt && lebendeNachbarzelle < 2 || lebt && lebendeNachbarzelle > 3){
+                    nextSpielfeld[i][j] = '0';
+                //Regel 3
+                } else if(lebt && lebendeNachbarzelle == 3 || lebt && lebendeNachbarzelle == 2){
+                    nextSpielfeld[i][j] = '9';
+                //Regel 4
+                } else {
+                    nextSpielfeld[i][j] = '0';
+                }
+            }
+        }
+    }
+
+
+
+    public static int countLebendeNachbarzellen(int z, int s, char[][] spielfeld){
+        int count = 0;
+        for(int i = 0; i < spielfeld.length; i++){
+            for(int j = 0; j < spielfeld[0].length; j++){
+                if(spielfeld[i][j] == '9'){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
 
     public static void main(String[] args) {
         SchischVisualizer sv = new SchischVisualizer();
         initiSpielfeld(10, 10);
         sv.step(spielfeld);
 
-        Random(0.9);
+        Random(0.4);
+        sv.step(spielfeld);
+
+
         sv.step(spielfeld);
 
 
